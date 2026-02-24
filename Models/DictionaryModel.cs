49089215b23b1cdc5c0f7decd0de4c94ed5f234c
Innokentiy945 <forjimmy945@gmail.com>
2026-02-24 23:30:23 +0100

    updated

--- Models/DictionaryModel.cs
+++ Models/DictionaryModel.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
 
 namespace StaApi.Models;
 
@@ -8,13 +9,17 @@ public class DictionaryModel
 {
     [Key]
     public Guid Id { get; set; }
-    
+
+    [Required]
     [Column("word")]
+    [JsonProperty("word_lat")]
     public string Word { get; set; }
-    
+
     [Column("definition")]
+    [JsonProperty("definition")]
     public string Definition { get; set; }
 
     [Column("part_of_speech")]
+    [JsonProperty("pos")]
     public string Pos { get; set; }
 }
