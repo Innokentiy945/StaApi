34f6abe097538f962db4959b44976bfeaf3a2f21
Innokentiy945 <forjimmy945@gmail.com>
2026-03-30 23:50:29 +0200

    pdated conn string type

--- Program.cs
+++ Program.cs
@@ -27,8 +27,8 @@ string DB_USER = Environment.GetEnvironmentVariable("DB_USER")
 string DB_PASSWORD = Environment.GetEnvironmentVariable("DB_PASSWORD") 
                      ?? throw new Exception("DB_PASSWORD not found");
 
-string connStr = $"Server={DB_HOST};Port={DB_PORT};Database={DB_NAME};User ID={DB_USER};Password={DB_PASSWORD};Charset=utf8;";
 
+string connStr = $"Host={DB_HOST};Port={DB_PORT};Database={DB_NAME};Username={DB_USER};Password={DB_PASSWORD};";
 //DbContext with MySQL/MariaDB
 builder.Services.AddDbContext<DictionaryContext>(options =>
     options.UseMySql(connStr, ServerVersion.AutoDetect(connStr))
